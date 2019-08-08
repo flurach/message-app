21c3013c6cce59b6e9cade1566f97e4aa8f08019
Aloure <aloure.ce@gmail.com>
2019-08-08 04:47:00 -0700

    Init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+
+    console.log('Someone is connected!');
+
+    socket.on('message', data => {
+        io.emit('message', data);
+    });
+
+    socket.on('disconnect', () => console.log('Someone is disconnected :('));
+
+});
+
+app.use(express.static('public'));
+http.listen(8080);
