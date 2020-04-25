d8865dc78a2ccebd21cd39672db795fbc013396f
Flurach <flurach.dev@gmail.com>
2020-04-25 12:01:22 +0300

    Fixed XSS issue

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 
 
+// methods
+const htmlEncode = s => mystring
+	.replace(/&/g, "&amp;")
+	.replace(/>/g, "&gt;")
+	.replace(/</g, "&lt;")
+	.replace(/"/g, "&quot;")
+
+
 // sockets
 io.on('connection', socket => {
 	console.log('Someone is connected!')
 
-	socket.on('message', data =>
-	    io.emit('message', data)
-	)
+	socket.on('message', data => {
+		data.content = htmlEncode(data.content)
+		io.emit('message', data)
+	})
 	socket.on('disconnect', () =>
 		console.log('Someone is disconnected :(')
 	)
