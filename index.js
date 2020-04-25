0a2887b7def30c076db84877eed61d955a02269e
Flurach <flurach.dev@gmail.com>
2020-04-25 11:26:42 +0300

    Last touches

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const PORT = 8080
-
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 
+
+// static files
+app.use(express.static('public'))
+
+
+// sockets
 io.on('connection', socket => {
-    console.log('Someone is connected!')
+	console.log('Someone is connected!')
 
-    socket.on('message', data => io.emit('message', data))
-
-    socket.on('disconnect', () => console.log('Someone is disconnected :('))
+	socket.on('message', data =>
+	    io.emit('message', data)
+	)
+	socket.on('disconnect', () =>
+		console.log('Someone is disconnected :(')
+	)
 })
 
-app.use(express.static('public'))
-http.listen(PORT, () => console.log(`Server listening on port ${8080}`))
+
+// listen
+const PORT = process.PORT || 8080
+http.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
